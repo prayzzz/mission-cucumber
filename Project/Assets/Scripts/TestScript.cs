5de969d3e56483a9d842df78e8f3491494259cb9
ZickZakk <gjenschmischek@gmail.com>
2014-10-19 14:44:02 +0200

    added docs and project folder

---
+++ Project/Assets/Scripts/TestScript.cs
@@ -0,0 +1,28 @@
+﻿namespace Assets.Scripts
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// The test script.
+    /// </summary>
+    public class TestScript : MonoBehaviour
+    {
+        #region Methods
+
+        /// <summary>
+        /// The start.
+        /// </summary>
+        private void Start()
+        {
+        }
+
+        /// <summary>
+        /// The update.
+        /// </summary>
+        private void Update()
+        {
+        }
+
+        #endregion
+    }
+}
