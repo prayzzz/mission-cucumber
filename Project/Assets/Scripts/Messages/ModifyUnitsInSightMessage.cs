72aef50cb7355518629eae74de346e8b7237c7ed
ZickZakk <gjenschmischek@gmail.com>
2014-10-28 16:26:28 +0100

    added SightComponent

---
+++ Project/Assets/Scripts/Messages/ModifyUnitsInSightMessage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+using Assets.Scripts.BaseClasses;
+
+namespace Assets.Scripts.Messages
+{
+    public class ModifyUnitsInSightMessage
+    {
+        public IList<BaseUnit> UnitsInSight { get; private set; }
+
+        public ModifyUnitsInSightMessage(IEnumerable<BaseUnit> unitsInSight)
+        {
+            this.UnitsInSight = new List<BaseUnit>(unitsInSight);
+        }
+    }
+}
---
+++ Project/Assets/Scripts/Messages/UnitsInSightMessage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+using Assets.Scripts.BaseClasses;
+
+namespace Assets.Scripts.Messages
+{
+    public class UnitsInSightMessage
+    {
+        public UnitsInSightMessage(IEnumerable<BaseUnit> unitsInSight)
+        {
+            this.UnitsInSight = unitsInSight;
+        }
+
+        public IEnumerable<BaseUnit> UnitsInSight { get; private set; }
+
+    }
+}
