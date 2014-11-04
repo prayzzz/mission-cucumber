5fd75c364eb057ffbcf4d2a2ac91e78b3c65991a
ZickZakk <gjenschmischek@gmail.com>
2014-11-04 21:49:31 +0100

    - added RepeatingInvoker

---
+++ Project/Assets/Scripts/Common/RepeatingInvoker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace Assets.Scripts.Common
+{
+    public class RepeatingInvoker : ScriptableObject
+    {
+        private float maxTime;
+
+        private float currentTime;
+
+        private Action actionToInvoke;
+        
+        public RepeatingInvoker(float interval, Action actionToInvoke)
+        {
+            this.currentTime = 0;
+            this.maxTime = interval;
+            this.actionToInvoke = actionToInvoke;
+            this.IsRunning = false;
+        }
+
+        public bool IsRunning { get; private set; }
+
+        public void Start()
+        {
+            this.currentTime = 0;
+            this.IsRunning = true;
+        }
+
+        public void Stop()
+        {
+            this.IsRunning = false;
+        }
+
+        public void Update()
+        {
+            if (!this.IsRunning)
+            {
+                return;
+            }
+
+            this.currentTime += Time.deltaTime;
+
+            if (this.currentTime < this.maxTime)
+            {
+                return;
+            }
+
+            this.actionToInvoke.Invoke();
+            this.currentTime = 0;
+        }
+    }
+}
